eeafc4ad7d1425bfba62f85a4bcd1afb68dfb24d
dale <dale@hmdtrucking.com>
2024-07-25 13:44:22 -0500

    added login page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for, session
 from flask_httpauth import HTTPBasicAuth
 import requests
 import os
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 auth = HTTPBasicAuth()
 
 users = {
@@ -19,9 +20,24 @@ def get_pw(username):
         return users.get(username)
     return None
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            session['logged_in'] = True
+            return redirect(url_for('get_employees'))
+        else:
+            return 'Invalid credentials', 401
+    return render_template('login.html')
+        
+
 @app.route('/employees', methods=['GET'])
-@auth.login_required
 def get_employees():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
+
     url = os.environ.get('BAMBOOHR_API_URL')
     token = os.environ.get('BAMBOOHR_API_TOKEN')
 
